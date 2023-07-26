85557c1cf894fa30a1739b8d1555466cacb8c02e
Evgenii Li <evgeniili2203@gmail.com>
2023-07-26 18:22:12 +0300

    added blank search result test

--- test_google_should_find_selene.py
+++ test_google_should_find_selene.py
@@ -17,3 +17,11 @@ def test_search_result(browser_opt):
     browser.open('')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in'))
+
+
+def test_blank_search_result(browser_opt):
+    browser.open('')
+    browser.element('[name="q"]').should(be.blank).type('0x000001DB9AA6C690').press_enter()
+    browser.element('[id="search"]').should(be.hidden)
+    browser.element('.card-section [aria-level="3"]').should(
+        have.text('Your search - 0x000001DB9AA6C690 - did not match any documents.'))
