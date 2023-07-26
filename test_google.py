1e7733cb1e7d4f628b2b0bf1ee641188c3df843c
Evgenii Li <evgeniili2203@gmail.com>
2023-07-26 21:59:27 +0300

    Minor code mistake fix

--- test_google.py
+++ test_google.py
@@ -3,14 +3,14 @@ from selene import be, have
 
 
 def test_search_result():
-    browser.open('')
+    browser.open()
     browser.element('[name=q]').should(be.blank).type('yashaka/selene').press_enter()
-    browser.element('#search').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+    browser.element('#search"').should(have.text('yashaka/selene: User-oriented Web UI browser tests in'))
 
 
 def test_blank_search_result():
     browser.open('')
-    browser.element('[name="q"]').should(be.blank).type('0x000001DB9AA6C690').press_enter()
+    browser.element('[name=q]').should(be.blank).type('0x000001DB9AA6C690').press_enter()
     browser.element('#search').should(be.hidden)
     browser.element('.card-section [aria-level="3"]').should(
         have.text('Your search - 0x000001DB9AA6C690 - did not match any documents.'))
