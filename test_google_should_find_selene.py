9c98d8a1feea6e125e3552d5e13d0b7d7aa5a268
Evgenii Li <evgeniili2203@gmail.com>
2023-07-26 18:35:36 +0300

    Optimized fixture

--- test_google_should_find_selene.py
+++ test_google_should_find_selene.py
@@ -10,17 +10,15 @@ options = webdriver.ChromeOptions()
 def browser_opt():
     options.add_argument("--window-size=1920,1080")
     browser.config.driver_options = options
-    browser.config.base_url = 'https://google.com/ncr'
+    browser.open('https://google.com/ncr')
 
 
 def test_search_result(browser_opt):
-    browser.open('')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in'))
 
 
 def test_blank_search_result(browser_opt):
-    browser.open('')
     browser.element('[name="q"]').should(be.blank).type('0x000001DB9AA6C690').press_enter()
     browser.element('[id="search"]').should(be.hidden)
     browser.element('.card-section [aria-level="3"]').should(
