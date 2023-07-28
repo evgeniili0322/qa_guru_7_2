0f926e7abaaa88bf408034052c68d8228e250f18
Evgenii Li <evgeniili2203@gmail.com>
2023-07-28 06:34:41 +0300

    delete unnecessary line

--- conftest.py
+++ conftest.py
@@ -9,6 +9,5 @@ options = webdriver.FirefoxOptions()
 def browser_opt():
     options.add_argument("-width=1920")
     options.add_argument("-height=1080")
-    browser.config.driver_name = 'firefox'
     browser.config.driver_options = options
     browser.config.base_url = 'https://google.com/ncr'
